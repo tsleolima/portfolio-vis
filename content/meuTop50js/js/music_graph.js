ec4530dc3a9169a52a0dd5295045f5f739532dbf
Leonardo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2018-03-16 23:53:06 -0300

    adicionando dados do top 50 spotify

--- content/meuTop50js/js/music_graph.js
+++ content/meuTop50js/js/music_graph.js
@@ -28,7 +28,7 @@ const legend = d3.legendColor()
     .classPrefix('legend');
 
 
-d3.json('top50leo.json', function (error, graph) {
+d3.json('https://raw.githubusercontent.com/tsleolima/portfolio-vis/master/content/meuTop50js/top50leo.json', function (error, graph) {
     if (error) throw error;
 
     const types = d3.set(graph.edges.map(e => e.type)).values();
